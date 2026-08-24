2696cc1db026ffc198b2ab6e4faa7975aeff16d5
Aditya Verma <9905adityaverma@gmail.com>
2026-08-24 23:24:27 +0530

    Time: 67 ms (18.87%), Space: 48 MB (18.48%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -4,22 +4,21 @@ class Solution {
         int n=s.length();
         int count=0;
         int ans=0;
-        int i=0;
+        int i=0; 
         int j=0;
         while(i<n){
             char c=s.charAt(i);
-            if(!set.contains(c)){
-                set.add(c);
-                count++;
-                i++;
-            }else{
+            if(set.contains(c)){
                 set.remove(s.charAt(j));
                 j++;
                 count--;
+            }else{
+                set.add(c);
+                count++;
+                i++;
             }
             ans=Math.max(ans, count);
         }
-        
         return ans;
     }
 }
