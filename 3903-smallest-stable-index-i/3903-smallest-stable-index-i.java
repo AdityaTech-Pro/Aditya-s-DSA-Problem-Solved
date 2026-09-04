c8ea119c7168ec5a68159575b57d8dfb3dc9cd55
Aditya Verma <9905adityaverma@gmail.com>
2026-09-04 18:27:30 +0530

    Time: 2 ms (11.49%), Space: 46.1 MB (88.3%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
@@ -1,6 +1,5 @@
 class Solution {
     public int firstStableIndex(int[] nums, int k) {
-        HashSet<Integer>set=new HashSet<>();
         int ResindexVal=Integer.MAX_VALUE;
         int Resindex=-1;
         int n=nums.length;
@@ -9,25 +8,16 @@ class Solution {
         for(int i=0; i<n; i++){
             arr[idx++]=check(nums, i);
         }
-        for(int i=0; i<n; i++){
-            System.out.print(arr[i]);
-        }
         for(int i=0; i<arr.length; i++){
             if(arr[i]<=k){
-                return i;
-                // if(set.contains(arr[i])){
-                //     continue;
-                // }
-                // ResindexVal=Math.min(ResindexVal, ai);
-                // if(ResindexVal==arr[i]){
-                //     Resindex=i;
-                // }
-                // set.add(arr[i]);
+                Resindex=i;
+                break;
             }
         }
         
         return Resindex;
     }
+
     public int check(int[] nums, int index){
         int max=Integer.MIN_VALUE;
         int min=Integer.MAX_VALUE;
