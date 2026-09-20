e35cf94472e369a38dfa645b886dfb06c299c333
Aditya Verma <9905adityaverma@gmail.com>
2026-09-20 22:40:11 +0530

    Time: 1552 ms (9.55%), Space: 49.3 MB (5.63%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,46 +1,5 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        // int n=nums.length;
-        // int[] ps=new int[n];
-        // ps[0]=nums[0];
-        // for(int i=1; i<n; i++){
-        //     ps[i] =ps[i-1] + nums[i];
-        // }
-
-        // int count=0;
-        
-        // for(int i=0; i<n; i++){
-        //     if(ps[i]==k){
-        //         count++;
-        //     }
-        //     for(int j=i+1; j<n; j++){
-        //         if(ps[j]-ps[i] == k && i != j){
-        //             count++;
-        //         }
-        //     }
-        // }
-       
-        // return count;
-
-    //     int n=nums.length;
-    //     int count=0;
-    //     for(int i=0; i<n; i++){
-    //         for(int j=i; j<n; j++){
-    //             if(k==call(nums,i,j)){
-    //                 count++;
-    //             }
-    //         }
-    //     }
-    //     return count;
-    // }
-    // public int call(int[] a, int i, int j){
-    //     int sum=0;
-    //     for(int k=i; k<=j; k++){
-    //         sum+=a[k];
-    //     }
-    //     return sum;
-    // }
-//      
         int count=0;
         int n=nums.length;
         int[] prefix=new int[n];
